8c1ab545c16615b1f126816654777ad74485e4ba
trung.nguyendinh <127390593+dinhtrungndt@users.noreply.github.com>
2023-12-03 11:05:18 +0700

    Initial commit

---
+++ src/components/home/canhan/index.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const CaNhanScreen = () => {
+  return (
+    <View>
+      <Text>CaNhanScreen</Text>
+    </View>
+  );
+};
+
+export default CaNhanScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/components/home/danhba/index.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const DanhBaScreen = () => {
+  return (
+    <View>
+      <Text>DanhBaScreen</Text>
+    </View>
+  );
+};
+
+export default DanhBaScreen;
+
+const styles = StyleSheet.create({});
