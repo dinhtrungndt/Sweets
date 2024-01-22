f8f790722fd3da2a251f6c3137371c23aa6250df
HuuDung <115625611+huydung7a5@users.noreply.github.com>
2024-01-22 17:16:17 +0700

    Update 22/1/2024
    Update Search user, bài viết user

--- src/components/main/layout/search/index.js
+++ src/components/main/layout/search/index.js
@@ -1,15 +1,64 @@
 /* eslint-disable prettier/prettier */
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Post from './Src/Post/Post';
+import User from './Src/User/User';
+import styles from './Styles/Index';
 
-const SearchScreens = () => {
+const TabTop = createMaterialTopTabNavigator();
+const options = ({ route }) => ({
+  tabBarLabel: ({ focused }) => {
+    const labelColor = focused ? 'blue' : '#000';
+    return (
+      <Text style={{ color: labelColor }}>
+        {route.name === 'All' ? 'Tất cả' : route.name === 'User' ? 'User' : 'Bài đăng'}
+      </Text>
+    );
+  },
+  tabBarIndicatorStyle: {
+    backgroundColor: 'blue',
+    height: 2,
+  },
+  tabBarPressAnimation: true,
+});
+const HomeTabsTop = ({ route }) => {
+  route = route;
   return (
-    <View>
-      <Text>SearchScreens</Text>
-    </View>
+    <TabTop.Navigator tabBarOptions={options}  >
+      <TabTop.Screen name="User" component={User} initialParams={route} />
+      <TabTop.Screen name="Post" component={Post} initialParams={route} />
+    </TabTop.Navigator>
   );
 };
 
+const SearchScreens = () => {
+  const navigation = useNavigation();
+  const [search, setSearch] = useState('');
+  const handleBackPress = () => {
+    navigation.goBack();
+  };
+  useEffect(() => {
+    navigation.setParams({ search });
+  }, [search]);
+  const handlesearch = () => {
+    navigation.setParams({ search });
+    navigation.navigate('Post');
+    navigation.navigate('User');
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={handleBackPress}>
+          <Icon name="chevron-back" size={30} color="black" />
+        </TouchableOpacity>
+        <TextInput value={search} onChangeText={setSearch} placeholder='Tìm kiếm' style={styles.headerinput} onSubmitEditing={handlesearch} />
+      </View>
+      <HomeTabsTop route={search} />
+    </View>
+  );
+};
 export default SearchScreens;
 
-const styles = StyleSheet.create({});
