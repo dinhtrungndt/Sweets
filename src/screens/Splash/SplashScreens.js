b7b9bd0b2fb3b3b8092acdf4f0bd9bc008ecd10f
HuuDung <115625611+huydung7a5@users.noreply.github.com>
2024-01-26 13:06:09 +0700

    Lỗi
    đừng lấy

--- src/screens/Splash/SplashScreens.js
+++ src/screens/Splash/SplashScreens.js
@@ -1,13 +1,14 @@
-/* eslint-disable prettier/prettier */
-import {Text, View} from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const SplashScreens = () => {
   return (
     <View>
       <Text>SplashScreens</Text>
     </View>
-  );
-};
+  )
+}
 
-export default SplashScreens;
+export default SplashScreens
+
+const styles = StyleSheet.create({})
