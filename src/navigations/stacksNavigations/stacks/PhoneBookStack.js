b0337662ef8271c48e2f2fdac79ec80536088502
nhatduy2210 <tenduyhole@gmail.com>
2024-04-04 11:28:00 +0700

    up

--- src/navigations/stacksNavigations/stacks/PhoneBookStack.js
+++ src/navigations/stacksNavigations/stacks/PhoneBookStack.js
@@ -20,7 +20,7 @@ import ScanQR from '../../../components/main/phonebook/Feature/ScanQR';
 const Stack = createNativeStackNavigator();
 
 export function PhoneBookStack() {
-  const navigation = useNavigation();
+ // const navigation = useNavigation();
   return (
     <Stack.Navigator
       screenOptions={{
